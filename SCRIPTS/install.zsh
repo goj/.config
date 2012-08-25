7dc055b52f8701f4cc1d4b2e8e273a1f9aa9ddc4
Krzysztof Goj <krzysztof.goj@gmail.com>
2012-08-25 13:53:44 +0200

    Allow vim to fetch deps via script

--- SCRIPTS/install.zsh
+++ SCRIPTS/install.zsh
@@ -1,2 +1,3 @@
 #!/bin/zsh
-foreach file (`pwd`/../LEGACY/*) ln -sT $(readlink -f $file) $HOME/.$(basename $file); end
+foreach file (`pwd`/LEGACY/*) ln -sT $(readlink -f $file) $HOME/.$(basename $file); end
+(cd vim && ./get-deps.sh)
